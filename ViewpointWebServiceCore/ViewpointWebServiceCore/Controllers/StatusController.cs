9de62b8c050eea3e0f4ffbf573eae75fd3403a96
Jessie <jasuncion@willdan.com>
2020-08-15 15:29:57 -0700

    Added Swagger, StatusController with authorization

---
+++ ViewpointWebServiceCore/ViewpointWebServiceCore/Controllers/StatusController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ViewpointWebServiceCore.Controllers
+{
+
+    public class StatusController : ControllerBase
+    {
+        [HttpGet()]
+        [Route("api/Status")]
+        [Authorize]
+        public IActionResult Test()
+        {
+            string message = "Hello World";
+            return Ok(new { message });
+        }
+    }
+}
--- ViewpointWebServiceCore/ViewpointWebServiceCore/Startup.cs
+++ ViewpointWebServiceCore/ViewpointWebServiceCore/Startup.cs
@@ -1,15 +1,16 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Protocols;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 
 namespace ViewpointWebServiceCore
 {
@@ -26,6 +27,61 @@ namespace ViewpointWebServiceCore
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1 " + DateTime.Now.ToString(),
+                    Title = "OnlinePortalWebServiceCore",
+                    Description = "ASP.NET Core Web API",
+                    TermsOfService = new Uri("https://example.com/terms"),
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Authors: Willdan Strategic Development Team",
+                        //Email = "Contacts: JWorrall@willdan.com, JAsuncion@Willdan.com, VYekklurBalaji@willdan.com",
+                        Url = new Uri("https://Wiildan.com/"),
+                    }
+                });
+            });
+
+            services.AddSwaggerGenNewtonsoftSupport();
+
+            var issuer = "https://dev-317836.okta.com/oauth2/default";
+            //var issuer = "https://dev-878852.okta.com/oauth2/default";
+            var configurationManager = new ConfigurationManager<OpenIdConnectConfiguration>(
+                        issuer + "/.well-known/oauth-authorization-server",
+                        new OpenIdConnectConfigurationRetriever(),
+                        new HttpDocumentRetriever());
+
+            var signingKeys = Task.Run(async () =>
+            {
+                OpenIdConnectConfiguration discoveryDocument = await configurationManager.GetConfigurationAsync(default(CancellationToken));
+                return discoveryDocument.SigningKeys;
+            }).GetAwaiter().GetResult();
+
+            services.AddAuthentication(cfg =>
+            {
+                cfg.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                cfg.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(cfg =>
+            {
+                cfg.TokenValidationParameters = new TokenValidationParameters
+                {
+                    RequireExpirationTime = true,
+                    RequireSignedTokens = false,
+                    ValidateAudience = true,
+                    ValidAudience = "api://default",
+                    ValidateIssuer = true,
+                    ValidIssuer = issuer,
+                    ValidateIssuerSigningKey = false,
+                    IssuerSigningKeys = signingKeys,
+                    ValidateLifetime = true,
+                    ClockSkew = TimeSpan.FromMinutes(2)
+                };
+
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -40,11 +96,21 @@ namespace ViewpointWebServiceCore
 
             app.UseRouting();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+            });
+
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                string swaggerJsonBasePath = string.IsNullOrWhiteSpace(c.RoutePrefix) ? "." : "..";
+                c.SwaggerEndpoint($"{swaggerJsonBasePath}/swagger/v1/swagger.json", "OnlinePortalWebApiCore");
             });
         }
     }
